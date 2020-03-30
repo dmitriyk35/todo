7c61df6c71734da17961b980eaaac64901b1abf9
dmitriyk35 <54919299+dmitriyk35@users.noreply.github.com>
2020-03-30 16:58:06 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,26 +8,64 @@ class TodoList extends Component {
       todos: [],
       currentToDo: "",
     };
+    this.deleteItem = this.deleteItem.bind(this)
   }
+
 
   handleChange = (event) => {
     console.log(event.target.value)
+    // const target = event.target;
+    // const value = target.type === 'checkbox' ? target.checked : target.value;
+    // // const name = target.currentToDo;
+
+    this.setState({
+      currentToDo: event.target.value,
+    });
+    // console.log('Change detected. State updated ' + ' = ' + value);
   };
+
+
 
   addItem = (event) => {
     event.preventDefault();
+    this.state.todos.push(this.state.currentToDo)
+    this.setState(
+      this.state
+    );
     console.log("addItem Method fired");
+  };
+
+  deleteItem = (index) => {
+  // const todos = this.state.todos.filter((todo, todoIndex) => {
+  //   return todoIndex !== index
+  // })
+  // this.state.todos.splice(-1)
+    // this.state.todos.splice(-1)
+    this.setState(
+      this.state.todos.splice(-1)
+    );
   };
 
 
   render() {
     return (
       <div className="container">
+      {console.log(this.state.todos)}
         <form onSubmit={this.addItem}>
-          <label htmlFor="tastName">Task Name:</label>
+          <label htmlFor="taskName">Task Name:</label>
           <input onChange={this.handleChange} name="tastName" type="text" placeholder="Add todo here!" />
           <button type="submit">Add Task</button>
         </form>
+
+        <ul>
+        {this.state.todos.map(todos => {
+          return (
+            <li onClick={this.deleteItem}>
+              {todos} <button>Delete</button>
+            </li>
+          );
+        })}
+        </ul>
       </div>
     );
   }
